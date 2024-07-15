d8fea743841d65ae938bfa7ce0979513fc7b84fa
line-chammai <line.chammai@mail.polimi.it>
2024-07-15 15:54:34 +0200

    new team page

--- pages/team.vue
+++ pages/team.vue
@@ -3,9 +3,70 @@ const { data: people } = await useFetch('/api/people')
 </script>
 
 <template>
-  <div>Our Team</div>
+  <div class="title">Our Team</div>
 
-  <NuxtLink v-for="person in people" :to="'/person/'+person.id">{{ person }}</NuxtLink>
+  <div class="container">
+    <NuxtLink v-for="person in people" :to="'/person/'+person.id">
+      <div class="team-member">
+        <img src="" alt="Team Member">
+        <h3>{{ person.name }}</h3>
+        <p>{{ person.role }}</p>
+        <p>{{ person.education }}</p>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+  font-size: 30pt;
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+}
+.container {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+.team-member {
+  width: 200px;
+  height: 300px;
+  border-radius: 10px;
+  background-color: #9E8DB5;
+  text-align: center;
+  margin: 0 auto;
+  padding: 10px;
+}
+.team-member img {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  margin-top: 10px;
+}
+.team-member h3 {
+  margin-top: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.team-member p {
+  margin-top: 5px;
+  font-size: 14px;
+}
+.back {
+  background-color: #d3d3d3;
+  width: 200px;
+  height: 50px;
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  border-radius: 5px;
+}
+.back i {
+  color: white;
+  font-size: 20px;
+  line-height: 50px;
+  position: absolute;
+  left: 30px;
+}
+</style>
